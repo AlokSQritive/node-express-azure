5e187fbdeb83a41cdc62f181b1c38ac1e9ae51b5
Alok Shrivastava <alok.shrivastava@qritive.com>
2022-01-17 03:04:58 +0530

    Title changed

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Anansi AI module' });
+  res.render('index', { title: 'Anansi AI Module' });
 });
 
 module.exports = router;
